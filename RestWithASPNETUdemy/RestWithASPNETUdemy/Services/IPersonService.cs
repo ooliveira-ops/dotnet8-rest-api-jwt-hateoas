99ec85f6d61d1e8bf11364c8b20e9b27e1b1a9e3
ooliveira-ops <filipecordeirooliveira838@gmail.com>
2026-07-27 14:35:03 -0300

    feat: Adicionando classe controller Person,Model e a Interface

---
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -0,0 +1,14 @@
+﻿using RestWithASPNETUdemy.Model;
+
+namespace RestWithASPNETUdemy.Services
+{
+	public interface IPersonService
+	{
+		Person Create (Person person);
+		Person FindById (long id);
+		Person FindByName (string name);
+		List<Person> FindAll ();
+		Person Update (Person person);
+		void Delete (long id);
+	}
+}
