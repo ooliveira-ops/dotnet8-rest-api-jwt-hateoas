c264c3af31fb71dba6d96351233926d1e5ab4d9b
ooliveira-ops <filipecordeirooliveira838@gmail.com>
2026-07-27 14:49:13 -0300

    feat: Implementando a Interface IPersonService

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Services/IPersonService.cs
@@ -6,7 +6,6 @@ namespace RestWithASPNETUdemy.Services
 	{
 		Person Create (Person person);
 		Person FindById (long id);
-		Person FindByName (string name);
 		List<Person> FindAll ();
 		Person Update (Person person);
 		void Delete (long id);
