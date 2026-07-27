5ad8ed38ee8a58d6eaa2c9dcd52f584298992824
ooliveira-ops <filipecordeirooliveira838@gmail.com>
2026-07-27 13:42:51 -0300

    feat: Estrutura basica da calculadora

--- RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
+++ RestWithASPNETUdemy/RestWithASPNETUdemy/Program.cs
@@ -1,19 +1,20 @@
+using Microsoft.AspNetCore.Builder;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddControllers();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
-    app.MapSwagger();
 }
 
 app.UseHttpsRedirection();
+
+app.MapControllers();
 
 var summaries = new[]
 {
